77e23204b5a26f0813df62ec7867b89f8983ef68
Pietro <pietro.barbiero9@live.it>
2020-02-15 16:52:27 +0100

    Simplified tests

--- alphazero/tests/test_connect4.py
+++ alphazero/tests/test_connect4.py
@@ -22,7 +22,7 @@ class TestConnect4(unittest.TestCase):
                             help="Number of games to simulate per MCTS self-play process")
         parser.add_argument("--temperature_MCTS", type=float, default=1.1,
                             help="Temperature for first 10 moves of each MCTS self-play")
-        parser.add_argument("--num_evaluator_games", type=int, default=100,
+        parser.add_argument("--num_evaluator_games", type=int, default=1,
                             help="No of games to play to evaluate neural nets")
         parser.add_argument("--neural_net_name", type=str, default="cc4_current_net_", help="Name of neural net")
         parser.add_argument("--batch_size", type=int, default=32, help="Training batch size")
@@ -47,88 +47,6 @@ class TestConnect4(unittest.TestCase):
                     counts += 1
                     learn(args, AlphaNet, Connect4, iteration=i, new_optim_state=True)
                     winner = evaluate(args, i, i + 1, AlphaNet, Connect4)
-        print()
-
-    def test_self_play(self):
-
-        from alphazero import Connect4
-        from alphazero import AlphaNet
-        from alphazero import self_play
-        import torch
-        import logging
-        import os
-
-        num_games = 2   # number of games to simulate per MCTS self-play process
-        start_idx = 0
-        iteration = 0   # current iteration number to resume from
-        temperature_mcts = 1.1  # temperature for first 10 moves of each MCTS self-play
-        total_iterations = 10
-        neural_net_name = "cc4_current_net_"
-
-        c4game = Connect4()
-        net = AlphaNet(c4game)
-        cuda = torch.cuda.is_available()
-        if cuda:
-            net.cuda()
-
-        model_data_dir = "./model_data/"
-        if not os.path.isdir(model_data_dir):
-            os.makedirs(model_data_dir)
-
-        logging.info("Starting iteration pipeline...")
-        for i in range(iteration, total_iterations):
-
-            net_to_play = "%s_iter%d.pth.tar" % (neural_net_name, iteration)
-
-            logging.info("Preparing model for MCTS...")
-            net.eval()
-
-            current_net_filename = os.path.join(model_data_dir, net_to_play)
-            if os.path.isfile(current_net_filename):
-                checkpoint = torch.load(current_net_filename)
-                net.load_state_dict(checkpoint['state_dict'])
-                logging.info("Loaded %s model." % current_net_filename)
-            else:
-                torch.save({'state_dict': net.state_dict()}, os.path.join(model_data_dir, net_to_play))
-                logging.info("Initialized model.")
-
-            with torch.no_grad():
-                cpu = 0
-                self_play(net, c4game, num_games, start_idx, cpu, temperature_mcts, iteration)
-            logging.info("Finished MCTS!")
-
-    def test_game(self):
-
-        from alphazero import Connect4
-        from alphazero import AlphaNet
-        import torch
-
-        c4game = Connect4()
-        fig = c4game.view_game()
-        fig.show()
-        c4game.move(5)
-        fig = c4game.view_game()
-        fig.show()
-        winner = c4game.check_winner()
-        actions = c4game.actions()
-        print(winner)
-        print(actions)
-
-        encoded = c4game.encode_state()
-        decoded = c4game.decode_state(encoded)
-        encoded_t = encoded.transpose(2, 0, 1)
-        encoded_s = torch.from_numpy(encoded_t).float().cuda()
-
-        net = AlphaNet(c4game)
-        cuda = torch.cuda.is_available()
-        if cuda:
-            net.cuda()
-        net.eval()
-
-        child_priors, value_estimate = net(encoded_s)
-        child_priors = child_priors.detach().cpu().numpy().reshape(-1)
-        value_estimate = value_estimate.item()
-
         print()
 
 
