dbe0f3e4b30cdafe93abd04c0f30442a21d150c0
albertotonda <alberto.tonda@gmail.com>
2020-02-19 18:10:42 +0100

    Now script call_sabberstone hopefully works.

--- examples/call_sabberstone.py
+++ examples/call_sabberstone.py
@@ -10,9 +10,8 @@ def individual_file_to_commandline(ind_file):
         line = fp.readlines()[0]
         tokens = line.split(",")
         ind.append(float(tokens[3][2:])) # store first token as float, ignoring first character "["
-        for t in range(4, len(tokens)-1) :
+        for t in range(4, 4+20) :
             ind.append(float(tokens[t]))
-        ind.append(float(tokens[-1].split("]")[0]))
 
     #if ind is Individual get only the num_weights! else check that ind is a list of num_weights
     param = ""
@@ -21,25 +20,29 @@ def individual_file_to_commandline(ind_file):
     param = param[:-1]
     return param
 
+# as there are some issues with the SabberStone C# projects
+sabberstone_base_dir = os.path.abspath("../../SabberStone/core-extensions/SabberStoneCoreAi")
+os.chdir(sabberstone_base_dir)
 
-sabberstone_base_dir = "../../SabberStone"
 DECKS = ["RenoKazakusMage", "MidrangeJadeShaman" , "AggroPirateWarrior"]
 HERO_BY_DECK = {"RenoKazakusMage":"MAGE", "MidrangeJadeShaman":"SHAMAN" , "AggroPirateWarrior":"WARRIOR"}
 
-base_cmd_line = "dotnet run --project " + sabberstone_base_dir + "/core-extensions/SabberStoneCoreAi/SabberStoneCoreAi.csproj"
+base_cmd_line = "dotnet run --project SabberStoneCoreAi.csproj"
 
 # read individual and convert it to string
 INDIVIDUAL_BY_DECK = dict()
-INDIVIDUAL_BY_DECK["RenoKazakusMage"] = sabberstone_base_dir + "/core-extensions/SabberStoneCoreAi/results/inspyred-individuals-file-07092018-153823-ONLY-MAGE.csv"
-INDIVIDUAL_BY_DECK["MidrangeJadeShaman"] = sabberstone_base_dir + "/core-extensions/SabberStoneCoreAi/results/inspyred-individuals-file-07092018-153850-ONLY-SHAMAN.csv"
-INDIVIDUAL_BY_DECK["AggroPirateWarrior"] = sabberstone_base_dir + "/core-extensions/SabberStoneCoreAi/results/inspyred-individuals-file-07092018-153819-ONLY-WARRIOR.csv"
+INDIVIDUAL_BY_DECK["RenoKazakusMage"] = "results/inspyred-individuals-file-07092018-153823-ONLY-MAGE.csv"
+INDIVIDUAL_BY_DECK["MidrangeJadeShaman"] = "results/inspyred-individuals-file-07092018-153850-ONLY-SHAMAN.csv"
+INDIVIDUAL_BY_DECK["AggroPirateWarrior"] = "results/inspyred-individuals-file-07092018-153819-ONLY-WARRIOR.csv"
 
 d1 = "AggroPirateWarrior"
 d2 = "MidrangeJadeShaman"
+number_of_games = 20
 
 # rest of the command line
 cmd_line = base_cmd_line + " " + d1 + " " + HERO_BY_DECK[d1] + " " + individual_file_to_commandline(INDIVIDUAL_BY_DECK[d1]) + " "
 cmd_line += d2 + " " + HERO_BY_DECK[d2] + " " + individual_file_to_commandline(INDIVIDUAL_BY_DECK[d2]) + " "
+cmd_line += str(number_of_games)
 
 print(cmd_line)
 os.system(cmd_line)
